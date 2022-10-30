5cddda417350eb664f01634b348f3675971e4ccf
ed36080666 <ed36080666@gmail.com>
2022-10-30 01:13:00 -0400

    Formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,36 +22,32 @@
   </style>
 </head>
 <body>
-<div class="flex justify-center relative mt-10" id="app">
-  <div class="max-w-5xl text-center w-full flex flex-col gap-10">
-    <div class="text-6xl mb-5 mt-5">
-      Scraper
-    </div>
-
-    <div>
-      <content-scraper
-        :logo-map="logoMap"
-        csrf="{{ csrf_token() }}"
+  <div class="flex justify-center relative my-10" id="app">
+    <div class="max-w-5xl text-center w-full flex flex-col gap-10">
+      <div class="text-6xl mb-5 mt-5">
+        Scraper
+      </div>
+      <div>
+        <content-scraper
+          :logo-map="logoMap"
+          csrf="{{ csrf_token() }}"
+        />
+      </div>
+      <scrape-list-table
+        :items="videos"
+        @delete="onItemDelete"
       />
     </div>
-
-
-    <scrape-list-table
-      :items="videos"
-      @delete="onItemDelete"
-    />
-
   </div>
-</div>
 
-<script>
-  window.__INITIAL_STATE__ = @json([
-    'videos' => $videos,
-    'logo_map' => $logo_map
-  ]);
-</script>
+  <script>
+    window.__INITIAL_STATE__ = @json([
+      'videos' => $videos,
+      'logo_map' => $logo_map
+    ]);
+  </script>
 
-<script src="{{ mix('js/app.js') }}"></script>
+  <script src="{{ mix('js/app.js') }}"></script>
 
 </body>
 </html>
